3bbc4320c03b532fbaaf528f1a1a3629ae245e3a
ZIming Mao <ziming.mao@yale.edu>
2020-05-28 15:42:38 -0400

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+
+
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.patient == self}
+    end
+
+    def doctors
+        # appointments
+        appointments.map {|appointment| appointment.doctor}.uniq
+    end
+end
