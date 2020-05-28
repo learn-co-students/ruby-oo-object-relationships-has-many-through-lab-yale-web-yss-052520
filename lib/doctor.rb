08b35133ac9e49d3114ca56b34b85ff2eab38a68
davidyu7 <david.yu@yale.edu>
2020-05-28 16:13:24 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+
+    @@all = []
+
+    attr_reader :name
+
+    def self.all
+        @@all
+    end
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.doctor == self}
+    end
+
+    def patients
+        self.appointments.collect{|appointment| appointment.patient}.uniq
+    end
+
+end
