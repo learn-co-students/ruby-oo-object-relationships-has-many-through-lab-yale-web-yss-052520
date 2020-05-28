ca6db23285e12cdad8192630519ce342d8231208
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-28 18:25:42 -0400

    documentation

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,5 +1,6 @@
 # Patient has many doctors through appointments
 # All the info stored mainly in appointment and filtered out by patient and doctor
+# "The join class" - bringing together instances from the other two classes
 class Appointment
     @@all = []
     attr_accessor :date, :patient, :doctor
