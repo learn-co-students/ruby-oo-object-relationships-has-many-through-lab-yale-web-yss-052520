6c62d05d18c7dda73fa431259abab44bcfcb9451
Eric Ou <eric.ou@yale.edu>
2020-05-27 22:50:10 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save()
+    end
+
+    def songs()
+        Song.all().select{|song| song.artist == self}
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def genres()
+        songs().map {|song| song.genre}
+    end
+
+    def save()
+        self.class.all() << self
+    end
+
+    def self.all()
+        @@all
+    end
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save()
+    end
+    
+    def save()
+        self.class.all() << self
+    end
+
+    def appointments()
+        Appointment.all().select {|appointment| appointment.doctor == self}
+    end
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients()
+        appointments().map {|appointment| appointment.patient}
+    end
+    def self.all()
+        @@all
+    end
+end
